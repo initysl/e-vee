b1d4aeb31978b1ccc2f9e136968ffd880446d3f8
Yusuf Lawal <lawalyusufimp@gmail.com>
2025-12-30 13:12:57 +0100

    update: cors

--- server/app/main.py
+++ server/app/main.py
@@ -67,8 +67,8 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "https://e-vee.vercel.app",
         "http://localhost:3000",
-        "http://127.0.0.1:3000",
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
